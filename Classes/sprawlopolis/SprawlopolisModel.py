74e59f9942276d1c452049718613f3fbfa59246c
Marco Seibert <marco.seibert@gmx.net>
2026-02-05 08:21:29 +0100

    Revert "WIP"
    This reverts commit 8125872272d9253ab8f234731ef9b14e09e277fa.

--- Classes/sprawlopolis/SprawlopolisModel.py
+++ Classes/sprawlopolis/SprawlopolisModel.py
@@ -1,16 +1,12 @@
 from Classes.base.models import BaseModel, BaseCard
 import random
 
-from Classes.sprawlopolis.SprawlopolisBoardstate import SprawlopolisBoardstate
-
-
 class SprawlopolisModel(BaseModel):
     def __init__(self, game_data):
         super().__init__(game_data)
         self.score_cards = []
         self.hand_cards = []
         self.number_of_decks = 1
-        self.boardstate = SprawlopolisBoardstate
 
         # draw three scoring card
         for _ in range(3):
@@ -19,10 +15,7 @@ class SprawlopolisModel(BaseModel):
             self.score_cards.append(given_card)
 
         # draw first card
-        first_card = self.cards[0]
-        self.cards.remove(first_card)
-        self.boardstate.add_card_to_board(self, first_card, (30, 18))
-        # self.boardstate.play_card(card_id=first_card, position=(30, 18))
+        # self.boardstate.add_card_to_board(self.dict_of_decks[game].deal(), (30, 18))
 
         # draw three initial hand cards
         for _ in range(3):
